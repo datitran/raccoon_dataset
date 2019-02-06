ed17feb0725b33d3335434ce562c0da5b73e25d2
Ajinkya Chandrakant Bobade <ajinkyabobade93@gmail.com>
2019-02-06 10:39:50 +0530

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -29,7 +29,7 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'racoon':
+    if row_label == 'raccoon':
         return 1
     else:
         None
