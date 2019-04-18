373a19ae24ae00db420cbe5a5200268b83887c0c
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2019-04-18 01:55:59 -0300

    split is a private function now

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -14,23 +14,18 @@ from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
 
 
-
-# TO-DO replace this with label map
-def class_text_to_int(row_label):
-    if row_label == 'raccoon':
-        return 1
-    else:
-        None
-
-
-def split(df, group):
+def __split(df, group):
     data = namedtuple('data', ['filename', 'object'])
     gb = df.groupby(group)
-    return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
+    return [
+        data(filename, gb.get_group(x))
+        for filename, x in zip(gb.groups.keys(), gb.groups)
+    ]
 
 
-def create_tf_example(group, path):
-    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+def create_tf_example(group, path, class_dict):
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)),
+                        'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
@@ -66,20 +61,34 @@ def create_tf_example(group, path):
         classes_text.append(row['class'].encode('utf8'))
         classes.append(class_dict[row['class']])
 
-    tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(filename),
-        'image/source_id': dataset_util.bytes_feature(filename),
-        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-        'image/format': dataset_util.bytes_feature(image_format),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
-    }))
+    tf_example = tf.train.Example(
+        features=tf.train.Features(
+            feature={
+                'image/height':
+                dataset_util.int64_feature(height),
+                'image/width':
+                dataset_util.int64_feature(width),
+                'image/filename':
+                dataset_util.bytes_feature(filename),
+                'image/source_id':
+                dataset_util.bytes_feature(filename),
+                'image/encoded':
+                dataset_util.bytes_feature(encoded_jpg),
+                'image/format':
+                dataset_util.bytes_feature(image_format),
+                'image/object/bbox/xmin':
+                dataset_util.float_list_feature(xmins),
+                'image/object/bbox/xmax':
+                dataset_util.float_list_feature(xmaxs),
+                'image/object/bbox/ymin':
+                dataset_util.float_list_feature(ymins),
+                'image/object/bbox/ymax':
+                dataset_util.float_list_feature(ymaxs),
+                'image/object/class/text':
+                dataset_util.bytes_list_feature(classes_text),
+                'image/object/class/label':
+                dataset_util.int64_list_feature(classes),
+            }))
     return tf_example
 
 
