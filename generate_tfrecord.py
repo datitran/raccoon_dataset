1587934d094516684da91430ad1b40d9353f65b8
JemisaR <jessereinikka.work@gmail.com>
2019-06-18 17:27:36 +0300

    Improved reading label map to be more typo tolerant.

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -32,17 +32,18 @@ def read_label_map(label_map_path):
     item_id = None
     item_name = None
     items = {}
+	
     with open(label_map_path, "r") as file:
         for line in file:
-            line.strip()
-            if line == "item {":
+            line.replace(" ", "")
+            if line == "item{":
+                pass
+            elif line == "}":
                 pass
             elif "id" in line:
                 item_id = int(line.split(":", 1)[1].strip())
             elif "name" in line:
                 item_name = line.split(":", 1)[1].replace("'", "").strip()
-            elif line == "}":
-                pass
 
             if item_id is not None and item_name is not None:
                 items[item_name] = item_id
